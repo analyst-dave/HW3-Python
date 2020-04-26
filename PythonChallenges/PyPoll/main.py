29af110ba4b66453b79d1d581dcc9ae4dfd70179
remy177 <62580456+remy177@users.noreply.github.com>
2020-04-25 19:01:14 -0700

    Add files via upload

--- PythonChallenges/PyPoll/main.py
+++ PythonChallenges/PyPoll/main.py
@@ -2,15 +2,15 @@ import os
 import csv
 
 csvpath = os.path.join('.', 'Resources', 'election_data.csv')
+fh = open("PyPollOutput.txt", "w")
 
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
     winner = ""
     winning_votes = 0
@@ -30,18 +30,31 @@ with open(csvpath) as csvfile:
     print(f'Total Votes: {line_count}')
     print('-------------------------')
 
+    fh.write(f'Election Results\n')
+    fh.write(f'-------------------------\n')
+    fh.write(f'Total Votes: {line_count}\n')
+    fh.write(f'-------------------------\n')
+
     for role in candidates.keys():
+        
         for key in poll.keys():
             if poll.get(key) == role:
                 votes += 1
+
         if votes > winning_votes:
             winning_votes = votes
             winner = role
+
         temp = "{:.3%}".format(votes/line_count)
         print(f'{role}: {temp} ({votes})')
+        fh.write(f'{role}: {temp} ({votes})\n')
         votes = 0
 
     print("-------------------------")
     print(f'Winner: {winner}')
     print("-------------------------")
-    
+
+    fh.write("-------------------------\n")
+    fh.write(f'Winner: {winner}\n')
+    fh.write("-------------------------\n")
+    fh.close()
